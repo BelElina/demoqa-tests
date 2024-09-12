b712955e0a797afe700fec0f90cbc7011079d6c4
Belousova Elina <Elksy90@gmail.com>
2024-09-12 13:18:41 +0400

    Modified

--- src/test/java/tests/TestBoxTests.java
+++ src/test/java/tests/TestBoxTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -9,16 +10,28 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TestBoxTests {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
     @Test
     void fillFormTest() {
-        Configuration.pageLoadStrategy = "eager";
-        open("https://demoqa.com/text-box");
+        open("/text-box");
         $("#userName").setValue("Elksy");
         $("#userEmail").setValue("Elksy@maik.ru");
         $("#currentAddress").setValue("Seoul, South Korea");
         $("#permanentAddress").setValue("Seoul, South Korea");
         $("#submit").click();
 
-        $("#submit").shouldHave();
+        //$("#output").$("name").shouldHave(); -более длинная форма записи V
+        $("#output #email").shouldHave();
+        //$("#currentAddress",1); - возми вторую по счету айдишку currentAddress V
+        $("#output #currentAddress").shouldHave();
+        $("#output #permanentAddress").shouldHave();
+
     }
 }
