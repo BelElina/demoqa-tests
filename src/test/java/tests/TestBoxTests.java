2df2fa980b651dae90c4b97a9894ae2519c2e75e
Belousova Elina <Elksy90@gmail.com>
2024-09-12 12:23:05 +0400

    Added config to fix page load strategy

--- src/test/java/tests/TestBoxTests.java
+++ src/test/java/tests/TestBoxTests.java
@@ -1,14 +1,17 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class TestBoxTests {
 
     @Test
     void fillFormTest() {
+        Configuration.pageLoadStrategy = "eager";
         open("https://demoqa.com/text-box");
         $("#userName").setValue("Elksy");
         $("#userEmail").setValue("Elksy@maik.ru");
