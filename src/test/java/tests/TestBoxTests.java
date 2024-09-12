17884f327d356ba72e6bd28de9fd1aece6116569
Belousova Elina <Elksy90@gmail.com>
2024-09-12 12:13:10 +0400

    Added TestBoxTests.java

---
+++ src/test/java/tests/TestBoxTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTests {
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Elksy");
+        $("#userEmail").setValue("Elksy@maik.ru");
+        $("#currentAddress").setValue("Seoul, South Korea");
+        $("#permanentAddress").setValue("Seoul, South Korea");
+        $("#submit").click();
+
+        $("#submit").shouldHave();
+    }
+}
